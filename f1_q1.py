43b3067ab3b668f56a84b8810358a720a11ec058
EliRuan <61792997+EliRuan@users.noreply.github.com>
2020-08-31 19:07:05 -0300

    Delete f1_q1.py

--- f1_q1.py
+++
@@ -1,8 +0,0 @@
-# entrada
-velocidade_ms = float(input('digite uma velocidade em m_s: '))
-
-# processamento
-velocidade_kh = velocidade_ms * 3.6
-
-#saída
-print(f'A velocidade é {velocidade_kh} km/h')
