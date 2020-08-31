1774004809bdbd626e387eee2a46c6a5e5c37e51
EliRuan <61792997+EliRuan@users.noreply.github.com>
2020-08-31 19:11:11 -0300

    Delete f1_q19.py

--- f1_q19.py
+++
@@ -1,8 +0,0 @@
-#entrada
-raio_esfera = float(input('Digite o raio da esfera: '))
-
-#processamento
-volume_esfera = (4 * 3.14 * raio_esfera * 3) / 3
-
-#saída
-print('O volume da esfera é:', volume_esfera)
