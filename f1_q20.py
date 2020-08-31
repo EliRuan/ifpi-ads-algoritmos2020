492413773173ced8ac5ae66021dcea3d9656db06
EliRuan <61792997+EliRuan@users.noreply.github.com>
2020-08-31 19:11:37 -0300

    Delete f1_q20.py

--- f1_q20.py
+++
@@ -1,8 +0,0 @@
-#entrada
-temp_celsius = float(input('Digite a temperatura em °C: '))
-
-#processamento
-temp_fahr = (9 * temp_celsius + 160) / 5
-
-#saída
-print('A temperatura em °F é:', temp_fahr)
