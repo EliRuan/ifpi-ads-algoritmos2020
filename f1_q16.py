09612d295f9dd252a99cf431b63077fd3a976921
EliRuan <61792997+EliRuan@users.noreply.github.com>
2020-08-31 19:09:36 -0300

    Delete f1_q16.py

--- f1_q16.py
+++
@@ -1,9 +0,0 @@
-#entrada
-lado_do_quadrado = float(input('Digite o valor do lado do quadrado: '))
-
-#processamento
-area_do_quadrado = lado_do_quadrado * 2
-
-#saída
-print('A área do quadrado é:', area_do_quadrado)
-
