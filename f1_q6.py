b0508d55a2e2753c46bc7c3bea3289eda7fd07be
EliRuan <61792997+EliRuan@users.noreply.github.com>
2020-08-31 19:31:36 -0300

    Delete f1_q6.py

--- f1_q6.py
+++
@@ -1,8 +0,0 @@
-# entrada
-velocidade_km_h = int(input('digite uma velocidade em km/h: '))
-
-# processamento
-velocidade_ms = velocidade_km_h / 3.6
-
-#saída
-print(f'A velocidade é {velocidade_ms} m/s')
