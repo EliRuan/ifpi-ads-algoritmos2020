4d5559408334eaf8381f319b7834d898a8f2957d
EliRuan <61792997+EliRuan@users.noreply.github.com>
2020-08-31 19:11:25 -0300

    Delete f1_q2.py

--- f1_q2.py
+++
@@ -1,9 +0,0 @@
-# entrada
-horas = int(input('digite um valor em horas: '))
-minutos = int(input('digite o valor em minutos: '))
-
-#processamento
-minuto_total = (horas * 60) + minutos
-
-#saída
-print(f'o valor total em minutos é {minuto_total}')
