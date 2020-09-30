98fc33fdd0009ea1a751e5da69659c8ceb73747b
Eli Ruan Carvalho <61792997+EliRuan@users.noreply.github.com>
2020-09-30 17:04:38 -0300

    Add files via upload

---
+++ Strings/CamelCase.py
@@ -0,0 +1,13 @@
+def CamelCase ():
+    texto = input('Texto: ').strip()
+
+    qtd_palavra = 1
+    
+    for letra in s:
+        if letra.isupper():
+            qtd_palavra +=1
+        
+    print(qtd_palavra)
+
+
+CamelCase()
